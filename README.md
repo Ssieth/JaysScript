8727a8ccac8ba813fe875f0b09c33e55a6017adc
Ssieth Anabuki <38430835+Ssieth@users.noreply.github.com>
2024-06-19 14:16:43 +0100

    v0.3.1
    Configuration editing added

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Jays RPG Board Script
+## v0.3.1
+* Script configuration editing added
+
 ## v0.2.0
 * First release version.  Has speech styling and igame resizing.  Config is fixed.
--- jayrp.user.js
+++ jayrp.user.js
@@ -11,7 +11,7 @@
 // @grant       GM_setClipboard
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getResourceURL
-// @version     0.2.0
+// @version     0.3.1
 // @author      Ssieth
 // @description 19/06/2024, 10:33:25
 // @license     MIT
@@ -20,25 +20,25 @@
 
 // Setup config object
 let config = {};
-config.speechStyling = {};
-config.speechStyling.on = true;
-config.speechStyling.incQuote = true;
-config.speechStyling.CSS = "color: blue;";
-config.image = {}
-config.image.enlarge = true;
 
 // Setup page object
 let page = {}
 page["type"] = ""
 
 // -- What we are logging to the console -- //
-var logTags = {};
+let logTags = {};
 logTags.startup = true;
 logTags.functiontrace = false;
 
 // CSS
-var strCSSPointer = ".pointer {cursor: pointer !important; }";
-var strCSSMaxWidth100 = ".maxwidth100 {max-width: 100%}";
+let strCSSPointer = ".pointer {cursor: pointer !important; }";
+let strCSSMaxWidth100 = ".maxwidth100 {max-width: 100%}";
+
+// Contants
+let jayClass = "jayscript";
+
+// Script Info
+let scriptLoc = "https://github.com/Ssieth/JaysScript/raw/main/jayrp.user.js";
 
 // Logging
 function log(strLogTag, strMessage) {
@@ -52,6 +52,219 @@ function log(strLogTag, strMessage) {
     }
   }
 }
+
+/* =========================== */
+/* Main Menu                   */
+/* =========================== */
+function setupMenu(menuID,menuText,menuTarget,menuFunction) {
+  let $navMenu = $("div#navigation-menu ul");
+
+  // Add new menu
+  let $navItem = $(`<li id="menu-${menuID}" class="${jayClass}"><a href="${menuTarget}">${menuText}</a></li>`);
+  if (menuFunction) {
+    $navItem.click(function(e) {
+      e.preventDefault();
+      menuFunction();
+    });
+  }
+  $navMenu.append($navItem);
+}
+
+function mTestCallback() {
+  alert("WHoo!");
+}
+
+function setupMenus() {
+  // Remove old menus
+  let $navMenu = $("div#navigation-menu ul");
+  $(`.${jayClass}`).remove();
+
+  setupMenu("script","Script","/search#script");
+}
+
+/* =========================== */
+/* New Configuration Stuff     */
+/* =========================== */
+function initConfigCategory(configCat, catDisplayName, isAdmin) {
+  if (!config[configCat]) {
+      config[configCat] = {
+          loaded: new Date()
+      };
+  }
+  config_display[configCat] = {
+      displayName: catDisplayName,
+      isAdmin: !!isAdmin,
+      loaded: new Date()
+  }
+}
+
+function initConfigItem(itemCat, itemName, defaultValue, displaySettings) {
+  if (config[itemCat]) {
+      if (!config[itemCat].hasOwnProperty(itemName)) {
+          config[itemCat][itemName] = defaultValue;
+      }
+  }
+  config_display[itemCat][itemName] = displaySettings;
+}
+
+function loadConfig(andThen) {
+  var blNewConfig;
+  var strConf = GM_getValue("config","");
+  if (strConf === "") {
+      config = {};
+      blNewConfig = true;
+  } else {
+      config = JSON.parse(strConf);
+      blNewConfig = false;
+  }
+  if (andThen) andThen();
+  return !blNewConfig;
+}
+
+
+
+function saveConfig(andThen) {
+	config.version = GM_info.script.version;
+  config.savedWhen = new Date();
+  console.log("Saving config");
+  console.log(config);
+  GM_setValue("config",JSON.stringify(config));
+  if (andThen) andThen();
+}
+
+function initConfig(andThen) {
+  config = {};
+  config_display = {};
+  loadConfig();
+  // Settings categories
+  initConfigCategory("speechStyling","Speech Styling");
+  initConfigCategory("image","Images");
+  // Speech Styling
+  initConfigItem("speechStyling","on", true, {text: "Style Speech?", type: "bool" });
+  initConfigItem("speechStyling","incQuote", true , {text: "Include quotes?", type: "bool" });
+  initConfigItem("speechStyling","CSS", "color: blue;", {text: "Speech Styling (CSS)", type: "text" });
+  // Images
+  initConfigItem("image","enlarge", true, {text: "Click to Enlarge?", type: "bool" });
+  saveConfig();
+  if (andThen) andThen();
+}
+
+// Returns true if a setting hsa been set, false otherwise
+function updateConfig(controlID) {
+	var $control = $("#content div.content").find("#" + controlID);
+	var aID = controlID.split("-");
+	var catID = aID[3];
+	var settingID = aID[4];
+  console.log("Set: " + settingID);
+  console.log($control);
+	if ($control.hasClass("gm-settings-control-bool")) {
+		config[catID][settingID] = $control[0].checked;
+	} else if ($control.hasClass("gm-settings-control-int")) {
+        console.log("Inty: " + settingID);
+        var intVal = parseInt($control.val());
+        console.log(intVal);
+        if ($.isNumeric("" + intVal)) {
+            if (config_display[catID][settingID].hasOwnProperty("min") && intVal < config_display[catID][settingID].min) {
+                console.log("There's a minimum and " + intVal + " < " + config_display[catID][settingID].min);
+                return false;
+            }
+            if (config_display[catID][settingID].hasOwnProperty("max") && intVal > config_display[catID][settingID].max) {
+                console.log("There's a maximum and " + intVal + " > " + config_display[catID][settingID].max);
+                return false;
+            }
+            config[catID][settingID] = intVal;
+        } else {
+            console.log("Not an integer: " + $control.val())
+            return false;
+        }
+	} else {
+		config[catID][settingID] = $control.val();
+	}
+    return true;
+}
+
+function editConfig() {
+  GM_addStyle(".gm-settings-cat { float: left; display: block; border: thin solid black; padding: 10px; margin: 10px; background-color: #c5c5c5}");
+  GM_addStyle(".gm-settings-cat-title { font-weight: bold; font-size: 120%; margin-bottom: 10px; }");
+  GM_addStyle(".gm-settings-cat-settings { margin-left: 10px; }");
+  GM_addStyle(".gm-settings-setting { margin-bottom: 15px; border-bottom: thin solid gray; width: auto; padding-bottom: 5px; }");
+  GM_addStyle(".gm-settings-setting-label { margin-right: 10px; display: inline; font-weight: bold; color: black;}");
+  GM_addStyle(".gm-settings-control-int { width: 4rem; }");
+  GM_addStyle(".gm-settings-setting-label { max-width: 15rem; display: inline-block; }");
+  //var $page = $("div#helpmain");
+  //$("#fatal_error").css("width","auto");
+  let $titleBar = $("#content div.title-bar");
+  let $page = $("#content div.content");
+
+  console.log($titleBar);
+  $titleBar.html(`<h1>${GM_info.script.name} v${GM_info.script.version} (<a style='color:white' href='${scriptLoc}'>Force Update</a>)</h1>`)
+
+  $page.css("max-width","initial");
+  //var $title = $("<h2>Script Settings (v" + GM_info.script.version + ")</h2>");
+  $("h3.catbg").html("Script Settings (v" + GM_info.script.version + ")");
+  document.title = "Script Settings (v" + GM_info.script.version + ")";
+  $page.html("");
+  for (var key in config_display) {
+      var confd = config_display[key];
+      var $newcat = $("<div class='gm-settings-cat well' id='gm-settings-cat-" + key + "'></div>");
+      $newcat.append("<h3 class='gm-settings-cat-title'>" + confd.displayName + "</h3>");
+      var $newSettings = $("<div class='gm-settings-cat-settings'></div>");
+      for (var key2 in confd) {
+          var $newSetting = $("<div class='gm-settings-setting'></div>");
+          var setting = confd[key2];
+          var val = config[key][key2];
+          if (setting.text) {
+              $newSetting.append("<label class='gm-settings-setting-label' for='gm-settings-value-" + key + "-" + key2 + "'>" + setting.text + "</label>");
+              switch (setting.type) {
+                  case "bool":
+                      $newSetting.append("<span class='gm-settings-setting-value' style='display: inline' ><input type='checkbox' class='gm-settings-control gm-settings-control-bool' id='gm-settings-value-" + key + "-" + key2 + "'" + ((val) ? ' checked' : '') + "></span>");
+                      break;
+                  case "int":
+                      var min = '';
+                      if (setting.hasOwnProperty('min')) {
+                          min = " min='" + setting.min + "'";
+                      }
+                      var max = '';
+                      if (setting.hasOwnProperty('max')) {
+                          max = " max='" + setting.max + "'";
+                      }
+                      $newSetting.append("<span class='gm-settings-setting-value'><input type='number' class='gm-settings-control gm-settings-control-int' id='gm-settings-value-" + key + "-" + key2 + "' value='" + val + "'" + min + max + "></span>");
+                      break;
+                  case "select":
+                      var $select;
+                      $select = $("<select class='gm-settings-control gm-settings-control-select' id='gm-settings-value-" + key + "-" + key2 + "'>");
+                      for (var i = 0; i < setting.select.length; i++) {
+                          var selKey = setting.select[i];
+                          $select.append("<option value='" + selKey + "'" + ((val === selKey) ? " selected" : "") + ">" + selKey + "</option>");
+                      }
+                      $newSetting.append($select);
+                      break;
+                  case "text":
+                  default:
+                      $newSetting.append("<span class='gm-settings-setting-value'><input type='text' class='gm-settings-control gm-settings-control-text' id='gm-settings-value-" + key + "-" + key2 + "' value='" + val + "'></span>");
+                      break;
+              }
+              $newSettings.append($newSetting);
+          }
+      }
+      $newcat.append($newSettings);
+      if (!confd.isAdmin || strScriptAdmins.indexOf(user.id) > -1) {
+        $page.append($newcat);
+      }
+  }
+  $page.append("<div style='clear: both;'>&nbsp;</div>");
+  $page.find(".gm-settings-control").change($.debounce(500, function(e) {
+    if (updateConfig(e.target.id)) {
+            saveConfig(loadConfig);
+        };
+  }));
+  $page.find(".gm-settings-control-text, .gm-settings-control-int").keyup($.debounce(500, function(e) {
+    if (updateConfig(e.target.id)) {
+            saveConfig(loadConfig);
+        };
+  }));
+}
+/* =========================== */
 
 
 /* =========================== */
@@ -175,6 +388,16 @@ function applyCSS() {
   GM_addStyle(strCSSMaxWidth100);
 }
 
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
+ *
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
+
 /* =========================== */
 /* Page object                 */
 /* =========================== */
@@ -186,6 +409,15 @@ function getPage() {
   page.url.hash = window.location.hash;
   page.type = "";
   switch (page.url.page) {
+    case "search":
+      switch (page.url.hash) {
+        case "#script":
+          page.type = 'script';
+          break;
+        default:
+          page.type = page.url.page;
+      }
+      break;
     default:
       page.type = page.url.page;
       break;
@@ -199,6 +431,7 @@ function getPage() {
 function main() {
   applyCSS();
   getPage();
+  setupMenus();
   switch (page.type) {
       case "thread":
         StyleSpeechElements("div.message");
@@ -207,9 +440,14 @@ function main() {
       case "post":
         StyleSpeechElements("div.message");
         break;
+    case "script":
+        editConfig();
+        break;
   }
 }
 
 $( document ).ready(function() {
-    main();
+  console.log(GM_info);
+  log("startup", "Starting " + GM_info.script.name + " v" + GM_info.script.version);
+  initConfig(main);
 });
